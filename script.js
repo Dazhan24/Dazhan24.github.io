de5efa7059bf3eedee5a91eaf2ea2a6386b24f8a
Danish Hafizhan <faxletion@gmail.com>
2024-09-15 16:06:26 +0700

    Website baru

---
+++ script.js
@@ -0,0 +1,9 @@
+// Typing Text Code
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Adalah manusia biasa.', 'Nolep demi main game😂.', 'Suka turu.'],
+    typeSpeed: 65,
+    backSpeed: 65,
+    backDelay: 1200,
+    loop: true,
+  });   
